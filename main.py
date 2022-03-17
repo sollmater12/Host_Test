e9210852d6176bfd72c6f62c54b9f75e79e6274f
sollmater12 <kuba.andrey.jr@yandex.ru>
2022-03-17 17:29:16 +0300

    -init

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return '<h1>Миссия Колонизация Марса</h1>'
+    return '<link rel="icon" href="#"><h1>Миссия Колонизация Марса</h1>'
 
 
 @app.route('/index')
 def index():
-    return '<h1>И на Марсе будут яблони цвести!</h1>'
+    return '<link rel="icon" href="#"><h1>И на Марсе будут яблони цвести!</h1>'
 
 
 @app.route('/promotion')
 def promotion():
     check = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
              'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
-    return '</br>'.join(check)
+    return '<link rel="icon" href="#">' + str('</br>'.join(check))
 
 
 if __name__ == '__main__':

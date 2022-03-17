bf53752b5bd0f7f3c14a48bfcc1a1c3802efce36
sollmater12 <kuba.andrey.jr@yandex.ru>
2022-03-17 16:42:43 +0300

    -init

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def main():
+    return '<h1>Миссия Колонизация Марса</h1>'
+
+
+@app.route('/index')
+def index():
+    return '<h1>И на Марсе будут яблони цвести!</h1>'
+
+
+@app.route('/promotion')
+def promotion():
+    check = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+             'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(check)
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" 
+                            href="static/css_style/one_page.css">
+                            <title>Привет, Марс!</title>
+                          </head>
+                          <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="static/img/img.png" alt="здесь что-то было...а теперь нет">
+                            <br>Вот она какая, красная планета</br>
+                          </body>
+                        </html>"""
+
+@app.route('/promotion_image')
+def promotion_image():
+    return '''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                        <title>Привет, Яндекс!</title>
+                      </head>
+                      <body>
+                        <h1 class="text-danger">Жди нас, марс</h1>
+                        <img src="static/img/img.png" alt="здесь что-то было...а теперь нет">
+                        <div class="alert alert-dark" role="alert">
+                         Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                          Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                          Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                          И начнём с Марса!
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                          Присоединяйся!
+                        </div>
+                      </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm123')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    port = int(os.environ.get("POST", 5000))
+    app.run(host="0.0.0.0", port=port)

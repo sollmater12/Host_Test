d007637816d7ae40d0505641dbdcd8106d7878cf
sollmater12 <kuba.andrey.jr@yandex.ru>
2022-03-17 16:53:45 +0300

    -init

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -21,57 +19,6 @@ def promotion():
              'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
     return '</br>'.join(check)
 
-@app.route('/image_mars')
-def image_mars():
-    return f"""<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <link rel="stylesheet" type="text/css" 
-                            href="static/css_style/one_page.css">
-                            <title>Привет, Марс!</title>
-                          </head>
-                          <body>
-                            <h1>Жди нас, Марс!</h1>
-                            <img src="static/img/img.png" alt="здесь что-то было...а теперь нет">
-                            <br>Вот она какая, красная планета</br>
-                          </body>
-                        </html>"""
-
-@app.route('/promotion_image')
-def promotion_image():
-    return '''<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                        <link rel="stylesheet"
-                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-                        <title>Привет, Яндекс!</title>
-                      </head>
-                      <body>
-                        <h1 class="text-danger">Жди нас, марс</h1>
-                        <img src="static/img/img.png" alt="здесь что-то было...а теперь нет">
-                        <div class="alert alert-dark" role="alert">
-                         Человечество вырастает из детства.
-                        </div>
-                        <div class="alert alert-success" role="alert">
-                          Человечеству мала одна планета.
-                        </div>
-                        <div class="alert alert-secondary" role="alert">
-                          Мы сделаем обитаемыми безжизненные пока планеты.
-                        </div>
-                        <div class="alert alert-warning" role="alert">
-                          И начнём с Марса!
-                        </div>
-                        <div class="alert alert-danger" role="alert">
-                          Присоединяйся!
-                        </div>
-                      </body>
-                    </html>'''
-
 
 if __name__ == '__main__':
-    port = int(os.environ.get("POST", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(port=8080, host='127.0.0.1')
